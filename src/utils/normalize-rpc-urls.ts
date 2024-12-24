3aa35f4ee0850442432ff3c729dd02db55998440
Bram Borggreve <beeman@beeman.nl>
2024-12-24 02:59:06 +0000

    refactor: clean up normalize script

--- src/utils/normalize-rpc-urls.ts
+++ src/utils/normalize-rpc-urls.ts
@@ -1,15 +1,14 @@
 export function normalizeRpcUrls({ rpcUrl, rpcUrlSubscriptions }: { rpcUrl: string; rpcUrlSubscriptions?: string }) {
+  // Throw an error if rpcUrl is not provided
   if (!rpcUrl.length) {
     throw new Error('normalizeRpcUrls: rpcUrl is required')
   }
+
   // Return if both rpcUrl and rpcUrlSubscriptions are provided
   if (rpcUrlSubscriptions?.length) {
     return { rpcUrl, rpcUrlSubscriptions }
   }
-  // Use the default rpcUrlSubscriptions if rpcUrl is provided for localhost
-  if (rpcUrl === 'http://localhost:8899') {
-    return { rpcUrl, rpcUrlSubscriptions: rpcUrl.replace('http', 'ws').replace('8899', '8900') }
-  }
-  // In all other cases, replace the protocol
-  return { rpcUrl, rpcUrlSubscriptions: rpcUrl.replace('http', 'ws') }
+
+  // Return the rpcUrl and rpcUrlSubscriptions, replacing the protocol and port
+  return { rpcUrl, rpcUrlSubscriptions: rpcUrl.replace('http', 'ws').replace(':8899', ':8900') }
 }
